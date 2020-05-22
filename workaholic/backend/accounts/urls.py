d65d44b082be9bd427b71884931197d3dc137693
chinhockyang <hychycchin@gmail.com>
2020-05-22 21:03:36 +0800

    Commit 1.2: Basic register/index page
    - Created index and register page

--- workaholic/backend/accounts/urls.py
+++ workaholic/backend/accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatters = []
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.register, name='register')
+]
