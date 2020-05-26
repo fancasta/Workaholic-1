11ef54e017e5c9bdf79f18cd773ca2bf829581b4
chinhockyang <hychycchin@gmail.com>
2020-05-26 21:39:11 +0800

    Commit 1.2.2: Index page and Projects page structures
    Create Account:
    - Made changes such that a user of one email cannot make more than one accounts
    
    Index Page
    - Listed the projects that the user is a project_member of
    - Allow users to view projects, which directs them to project/home page
    
    Project/home page
    - Shows the list of users who are project-members of the project
    - Users that are not project-members of the project will not be able to see the project /home page

--- workaholic/backend/accounts/forms.py
+++ workaholic/backend/accounts/forms.py
@@ -3,9 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+from .models import Project
+
 class UserCreationForm_Edited(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-    
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
--- workaholic/backend/project/views.py
+++ workaholic/backend/project/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
 from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
 
+# Create your views here.
+from accounts.models import *
+
 from .decorators import user_is_project_member
 
-from accounts.models import *
+# Create your views here.
 
 @login_required
 @user_is_project_member
