6918577c1ec3c25c54b5ac28c8dbed00c6719598
chinhockyang <hychycchin@gmail.com>
2020-05-28 21:10:04 +0800

    2.1.1: Added To-Do feature in Project

--- workaholic/backend/project/views.py
+++ workaholic/backend/project/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
 from django.http import HttpResponse
+from django import forms
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 
@@ -15,6 +16,7 @@ from accounts.models import *
 
 from .decorators import user_is_project_member
 from .forms import *
+from .models import Todo
 
 # Create your views here.
 
@@ -23,8 +25,36 @@ from .forms import *
 def projectPage(request,pk):
     project = Project.objects.get(id=pk)
     members = project.project_members.all()
-    context = {'project':project, 'members':members}
+    if request.method == "POST":
+        todoform = TodoForm(request.POST)
+        added_by = members.get(user=request.user)
+        new_todo_text = todoform.data['todo']
+        new_todo = Todo(todo=new_todo_text, added_by=added_by ,project=project)
+        new_todo.save()
+        project.todo_set.add(new_todo)
+        return redirect('/project/' + str(pk) + '/')
+    else:
+        todoform = TodoForm()
+
+    todo = Todo.objects.filter(project=project)
+
+    context = {'project':project, 'members':members, 'todoform':todoform, 'todo':todo}
     return render(request, 'project/home.html', context)
+
+@login_required
+@user_is_project_member
+def deleteTodo(request, pk, todo_pk):
+    project = Project.objects.get(id=pk)
+    todo = project.todo_set.get(id=todo_pk)
+    deleteform = DeleteTodoForm(request.POST)
+    if request.method == 'POST' and deleteform.data:
+        todo.delete()
+        return redirect('/project/' + str(pk) + '/')
+    else:
+        deleteform = DeleteTodoForm()
+
+    context = {'project':project, 'todo':todo, 'deleteform': deleteform}
+    return render(request, 'project/delete_todo.html', context)
 
 @login_required
 @user_is_project_member
@@ -41,7 +71,6 @@ def addMembers(request,pk):
         except:
             messages.info(request,'Invalid user') 
             form = AddMemberForm()
-        
     else:
         form = AddMemberForm()
     
