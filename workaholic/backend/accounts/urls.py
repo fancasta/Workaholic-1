c9b733dfea17dfe0e0ad1478a01c72d07edbff37
chinhockyang <hychycchin@gmail.com>
2020-06-11 14:56:57 +0800

    Commit 2.5: Added basic password reset feature
    - Basic password reset feature using email link

--- workaholic/backend/accounts/urls.py
+++ workaholic/backend/accounts/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,14 @@ urlpatterns = [
     path('accounts/register/', views.registerPage, name='register'),
     path('accounts/login/', views.loginPage, name="login"),
     path('accounts/logout/', views.logoutPage, name="logout"),
-    path('accounts/create_project/', views.createProject, name="createProject")
+    path('accounts/create_project/', views.createProject, name="createProject"),
+
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done'),
+
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html'), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
